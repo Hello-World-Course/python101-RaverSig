a8a31dd914676abe8563842cfc51d1eb0f8c345f
Raver <alonw91@gmail.com>
2023-10-03 23:02:30 +0300

    second assignment.

--- project/ui/user_interaction.py
+++ project/ui/user_interaction.py
@@ -1,4 +1,4 @@
 name = input("Hello, whats your name?")
 print("Nice to meet you " + name)
-board_size =int(input(name + ", please choose the board size: "))
-mine_number = int(input(name + f", for board size {board_size}, choose number of mines to allocate: "))
+board_size =int(input(name + ", please choose board size:"))
+mine_number = int(input(name + f", for board size {board_size}, choose number of mines to allocate:"))
